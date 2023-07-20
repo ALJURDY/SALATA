df897657441736f61be6a8dc1b41e8ec5c312464
Clara Charriau <charriau.clara@gmail.com>
2023-07-20 16:08:21 +0200

    [CREATE] create mocks

---
+++ src/mocks/ingredients.ts
@@ -0,0 +1,107 @@
+export interface IIngredient {
+    id: number;
+    name: string;
+  }
+  
+  export const INGREDIENTS = [
+    {
+      id: 1,
+      name: "Mozarella"
+    },
+    {
+      id: 2,
+      name: "Viande hachée"
+    },
+    {
+      id: 3,
+      name: "Champignons"
+    },
+    {
+      id: 4,
+      name: "Poulet fumé"
+    },
+    {
+      id: 5,
+      name: "Chèvre"
+    },
+    {
+      id: 6,
+      name: "Boursin"
+    },
+    {
+      id: 7,
+      name: "Gorgonzola"
+    },
+    {
+      id: 8,
+      name: "Escalope"
+    },
+    {
+      id: 9,
+      name: "Kebab"
+    },
+    {
+      id: 10,
+      name: "Cordon Bleu"
+    },
+    {
+      id: 11,
+      name: "Merguez"
+    },
+    {
+      id: 12,
+      name: "Chorizo"
+    },
+    {
+      id: 13,
+      name: "Jambon"
+    },
+    {
+      id: 14,
+      name: "Lardons"
+    },
+    {
+      id: 15,
+      name: "Saumon"
+    },
+    {
+      id: 16,
+      name: "Crevettes"
+    },
+    {
+      id: 17,
+      name: "Câpres"
+    },
+    {
+      id: 18,
+      name: "Anchois"
+    },
+    {
+      id: 19,
+      name: "Thon"
+    },
+    {
+      id: 20,
+      name: "Herbes de Provence"
+    },
+    {
+      id: 21,
+      name: "Olives"
+    },
+    {
+      id: 22,
+      name: "Poivrons"
+    },
+    {
+      id: 23,
+      name: "Maïs"
+    },
+    {
+      id: 24,
+      name: "Pomme de terre"
+    },
+    {
+      id: 25,
+      name: "Oignons"
+    },
+  ]
