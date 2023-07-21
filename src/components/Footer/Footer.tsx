a03ae29332dbabd4dd74cc0e32827f8a41be455c
Clara Charriau <charriau.clara@gmail.com>
2023-07-21 16:09:01 +0200

    [CREATE] header & footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import style from "./Footer.module.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer className={style.footer}>
+    <div className={style.white}></div>
+    <Link to="/">
+        <img
+          className={style.logo}
+          src="/assets/logo/logo-salata.png"
+          alt="Logo Salata"
+        />
+      </Link>
+      <p className={style.copyright}>Réalisé par l’équipe des Pains d’Épices</p>
+   </footer>
   )
 }
 
-export default Footer
+export default Footer;
